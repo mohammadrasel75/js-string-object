2348cfb5613bddd03c70979f5c32b1eb90dd0d4f
mohammadrasel75 <devraselmiah75@gmail.com>
2026-02-20 23:18:19 +0600

    update string with

--- string.js
+++ string.js
@@ -6,5 +6,9 @@ const district = `B-Baria`;
 // console.log(thana);
 const numbers = [54, 65, 78, 98];
 
-console.log(numbers.length);
-console.log(numbers[0]);
+// console.log(numbers.length);
+// console.log(numbers[1]);
+
+const capital = 'Dha ka';
+console.log(capital.length);
+console.log(capital[0]);
