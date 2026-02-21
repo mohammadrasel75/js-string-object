f705097e1fa5fdd99446af74cadc0c1bb98c7342
mohammadrasel75 <devraselmiah75@gmail.com>
2026-02-22 00:22:35 +0600

    object update

--- object.js
+++ object.js
@@ -1,6 +1,38 @@
+// primitive 
+// basic,
+
+
 const age = 21;
 const school = 'rifle square';
 const isPassed = true; 
-let isDeveloper;
+// let isDeveloper;
 const subject = ['Bangla', 'English','Physics', 'Math'];
-console.log(isDeveloper);
+const bottle_wring = ['white', 45]
+const bottleColor = 'white';
+const bottlePrice = 45;
+const bottleDrinks = 'water';   
+
+// object
+const bottle = {
+    brand : 'RFL',
+    price : 45,
+    color : 'white',
+    isClear : false,
+
+}
+
+{
+ 
+// non-primitive data types    
+
+ const subject =  {
+    name : 'biology',
+    teacher : 'Rafiqul',
+    examDate : '30th Feb',
+    chapters : ['first', 'second', 'third'],
+    exams : {
+        name : 'final',
+        marks : 100,
+    }
+ }   
+}
